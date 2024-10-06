d688e4d5212ae4581bfd5f0eec3acc781cfcd7f3
Oleksii Vovnenko <kespa71for@gmail.com>
2024-10-06 16:19:59 +0200

    update Sidebar: add list project

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,11 +1,20 @@
 import Button from "./Button.jsx";
 
 const Sidebar = () => {
+    const projects = [];
+
     return (
         <aside
             className="w-1/3 text-center px-8 py-16 bg-stone-900 text-stone-50 md:w-72"
         >
             <Button>+ Add Project</Button>
+            <p className="py-2 text-xl text-start">Created projects</p>
+            <ul className="py-2 text-start">
+                {projects.length > 0
+                    ? projects.map((project, idx) => <li key={idx}>{project}</li>)
+                    : <li>Project list null.</li>
+                }
+            </ul>
         </aside>
     );
 };
