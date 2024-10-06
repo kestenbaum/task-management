6cf5210fcfb72c14e0c45a1bc3ec1e2f049bd667
Oleksii Vovnenko <kespa71for@gmail.com>
2024-10-06 16:05:57 +0200

    create custom Button

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
+import Button from "./components/Button.jsx";
+
 function App() {
   return (
-    <>
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
-    </>
+    <main className="h-screen my-8 flex gap-8">
+      <div>
+        <Button>Download</Button>
+      </div>
+    </main>
   );
 }
 
