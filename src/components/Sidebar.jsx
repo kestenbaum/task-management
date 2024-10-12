9ac7b400c04594013ce3786e6419a43f43f1fec2
kespa <kespa71for@gmail.com>
2024-10-13 00:25:43 +0200

    update Sidebar: add error

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,19 +4,22 @@ import ListItem from "./ListItem.jsx";
 
 const Sidebar = () => {
     const [project, setProject] = useState(["project", "project"]);
+
+    const projectItems = project.length > 0
+        ? <ul className="m-1">
+            {project.map((project, index) => (
+                <ListItem item={project} key={index}/>
+            ))}
+        </ul>
+        : <div className="py-1">You don*t have project</div>
+
     return (
         <aside
             className="w-1/3 text-center px-8 py-16 bg-stone-900 text-stone-50 md:w-72"
         >
             <p className="my-2 text-xl">Your projects</p>
             <Button>+ Add Project</Button>
-            {
-                <ul className="m-1">
-                    {project.map((project, index) => (
-                      <ListItem item={project} key={index} />
-                    ))}
-                </ul>
-            }
+            {projectItems}
         </aside>
     );
 };
