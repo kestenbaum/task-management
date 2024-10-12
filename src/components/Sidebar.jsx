5bb96d62d46a600ce8d8c8f9c98f2023c6629c89
kespa <kespa71for@gmail.com>
2024-10-13 00:22:06 +0200

    update Sidebar: add array projects

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,12 +1,22 @@
+import {useState} from "react";
 import Button from "./Button.jsx";
+import ListItem from "./ListItem.jsx";
 
 const Sidebar = () => {
+    const [project, setProject] = useState(["project", "project"]);
     return (
         <aside
             className="w-1/3 text-center px-8 py-16 bg-stone-900 text-stone-50 md:w-72"
         >
             <p className="my-2 text-xl">Your projects</p>
             <Button>+ Add Project</Button>
+            {
+                <ul className="m-1">
+                    {project.map((project, index) => (
+                      <ListItem item={project} key={index} />
+                    ))}
+                </ul>
+            }
         </aside>
     );
 };
