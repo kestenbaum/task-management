9d44cce8bd483ed2b7ef14470ab84c896238afa7
kespa <kespa71for@gmail.com>
2024-10-21 14:10:11 +0200

    update Sidebar: delete array project

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,25 +1,12 @@
-import {useState} from "react";
 import Button from "./Button.jsx";
-import ListItem from "./ListItem.jsx";
 
 const Sidebar = () => {
-    const [project, setProject] = useState(["project", "project"]);
-
-    const projectItems = project.length > 0
-        ? <ul className="m-1">
-            {project.map((project, index) => (
-                <ListItem item={project} key={index}/>
-            ))}
-        </ul>
-        : <div className="py-1">You don*t have project</div>
-
     return (
         <aside
             className="w-1/3 text-center px-8 py-16 bg-stone-900 text-stone-50 md:w-72"
         >
-            <p className="my-2 text-xl">Your projects</p>
+            <p className="my-2 text-xl">YOUR PROJECTS</p>
             <Button>+ Add Project</Button>
-            {projectItems}
         </aside>
     );
 };
