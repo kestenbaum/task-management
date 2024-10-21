dccda9bf1607f86778034c709c1fc34143daefce
kespa <kespa71for@gmail.com>
2024-10-21 15:25:56 +0200

    update Input : add forwardRef

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,15 +1,15 @@
-import React from 'react';
+import React, {forwardRef} from 'react';
 
-const Input = ({textarea, label, ...props}) => {
+const Input = forwardRef(function ({textarea, label, ...props}, ref) {
     const classes = "my-2 px-4 py-2 w-[250px] bg-stone-500 text-stone-100 cursor-pointer placeholder:text-stone-100 focus:placeholder:text-transparent";
     return (
         <p>
             <label>{label}</label>
             {textarea
-                ? <textarea className={classes} {...props} />
-                : <input className={classes} {...props} />}
+                ? <textarea ref={ref} className={classes} {...props} />
+                : <input ref={ref} className={classes} {...props} />}
         </p>
     );
-};
+});
 
 export default Input;
