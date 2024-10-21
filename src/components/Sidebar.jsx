ae3cb1d5dad110b1f17a33f146be0cc89edfd699
kespa <kespa71for@gmail.com>
2024-10-21 15:24:58 +0200

    update Sidebar: add function createProject

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,12 +1,12 @@
 import Button from "./Button.jsx";
 
-const Sidebar = () => {
+const Sidebar = ({onAddProject}) => {
     return (
         <aside
             className="w-1/3 text-center px-8 py-16 bg-stone-900 text-stone-50 md:w-72"
         >
             <p className="my-2 text-xl">YOUR PROJECTS</p>
-            <Button>+ Add Project</Button>
+            <Button onClick={onAddProject}>+ Add Project</Button>
         </aside>
     );
 };
