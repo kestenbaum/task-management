7e1cafe33ff83c07a1f786bd34bedf8b0e5980cd
kespa <kespa71for@gmail.com>
2024-10-21 14:24:47 +0200

    create NoProjectSelected:

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import Sidebar from "./components/Sidebar.jsx";
 import Form from "./components/Form.jsx";
+import NoProjectSelected from "./components/NoProjectSelected.jsx";
 
 function App() {
     return (
         <main className="h-screen flex gap-8">
             <Sidebar/>
-            <Form/>
+            <NoProjectSelected/>
         </main>
     );
 }
