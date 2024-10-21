00a29f7f118b49ef1e65f16bcc1657cc2c9c8e8b
kespa <kespa71for@gmail.com>
2024-10-21 15:26:34 +0200

    update Form : add refs for inputs and create new object

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,23 +1,49 @@
+import {useRef} from "react";
 import Button from "./Button.jsx";
 import Input from "./Input.jsx";
 
-const Form = () => {
+const Form = ({onCreate}) => {
+    const titleRef = useRef(null);
+    const descriptionRef = useRef(null);
+    const dateRef = useRef(null);
+
+
+    const handleSave = () => {
+        const titleValue = titleRef.current.value;
+        const descriptionValue = descriptionRef.current.value;
+        const dateValue = dateRef.current.value;
+
+        return onCreate({
+            title: titleValue,
+            description: descriptionValue,
+            date: dateValue,
+        })
+
+        //Validation ...
+    }
+
+
     return (
         <div className="flex flex-col justify-center items-center w-2/3 px-5 py-2">
             <div className="flex w-1">
                 <a className="px-4 py-2 text-xs md:text-base" href="">Clear</a>
-                <Button>Save</Button>
+                <Button onClick={handleSave}>Save</Button>
             </div>
             <form className="flex flex-col w-[250px]">
                 <Input
                     placeholder="Title"
+                    type="text"
+                    ref={titleRef}
                 />
                 <Input
-                    as="textarea"
+                    textarea
                     placeholder="Description"
+                    ref={descriptionRef}
                 />
                 <Input
                     placeholder="Due Date"
+                    type="date"
+                    ref={dateRef}
                 />
             </form>
         </div>
