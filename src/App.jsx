060a9b401d6f3423d62ba217ebb543becc880fdb
kespa <kespa71for@gmail.com>
2024-10-21 15:27:07 +0200

    update App : add function/ created new project

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,51 @@
+import {useState} from "react";
+
 import Sidebar from "./components/Sidebar.jsx";
-import Form from "./components/Form.jsx";
 import NoProjectSelected from "./components/NoProjectSelected.jsx";
+import Form from "./components/Form.jsx";
 
 function App() {
+    const [projectState, setProjectState] = useState({
+        projectStatus: undefined,
+        projects: [],
+    })
+
+    function handleAddProject() {
+        setProjectState(prevState => {
+            return {
+                ...prevState,
+                projectStatus: null
+            }
+        })
+    }
+
+    function handleCreateProject(projectDate) {
+        setProjectState(prevState => {
+
+            const newProject = {
+                ...projectDate,
+                id: Math.random().toString()
+            }
+
+            return {
+                ...prevState,
+                projects: [...prevState.projects, newProject]
+            }
+        })
+    }
+
+    let content;
+
+    if (projectState.projectStatus === null) {
+        content = <Form onCreate={handleCreateProject}/>
+    } else if (projectState.projectStatus === undefined) {
+        content = <NoProjectSelected onAddProject={handleAddProject}/>;
+    }
+    console.log(projectState)
     return (
         <main className="h-screen flex gap-8">
-            <Sidebar/>
-            <NoProjectSelected/>
+            <Sidebar onAddProject={handleAddProject}/>
+            {content}
         </main>
     );
 }
