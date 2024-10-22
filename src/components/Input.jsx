cce83156333ac070f011a3d48211f0d686c38975
kespa <kespa71for@gmail.com>
2024-10-22 02:19:20 +0200

    update Input:

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,4 +1,4 @@
-import React, {forwardRef} from 'react';
+import {forwardRef} from 'react';
 
 const Input = forwardRef(function ({textarea, label, ...props}, ref) {
     const classes = "my-2 px-4 py-2 w-[250px] bg-stone-500 text-stone-100 cursor-pointer placeholder:text-stone-100 focus:placeholder:text-transparent";
