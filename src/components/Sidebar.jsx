ea905839ebd629dc1b7b1e1c7493cf082bb549dd
kespa <kespa71for@gmail.com>
2024-10-22 02:19:03 +0200

    update Sidebar: add projects-array

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,12 +1,20 @@
 import Button from "./Button.jsx";
 
-const Sidebar = ({onAddProject}) => {
+const Sidebar = ({onAddProject, projects}) => {
     return (
         <aside
             className="w-1/3 text-center px-8 py-16 bg-stone-900 text-stone-50 md:w-72"
         >
             <p className="my-2 text-xl">YOUR PROJECTS</p>
             <Button onClick={onAddProject}>+ Add Project</Button>
+            <ul className="mt-5">
+                {projects.map((project, index) => <li
+                    key={index}
+                    className="p-1 text-start text-stone-50 cursor-pointer text-xs hover:text-stone-400"
+                >
+                    {project.title}
+                </li>)}
+            </ul>
         </aside>
     );
 };
