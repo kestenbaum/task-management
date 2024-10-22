239c46b1a42c90c971d49751365c70ec31c205f2
kespa <kespa71for@gmail.com>
2024-10-22 03:08:21 +0200

    update Form: add props for onClose function

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,7 +3,7 @@ import Button from "./Button.jsx";
 import Input from "./Input.jsx";
 import Modal from "./Modal.jsx";
 
-const Form = ({onCreate}) => {
+const Form = ({onCreate, onClose}) => {
     const titleRef = useRef(null);
     const descriptionRef = useRef(null);
     const dateRef = useRef(null);
@@ -39,7 +39,7 @@ const Form = ({onCreate}) => {
             </Modal>
             <div className="flex flex-col justify-center items-center w-2/3 px-5 py-2">
                 <div className="flex w-1">
-                    <a className="px-4 py-2 text-xs md:text-base" href="">Clear</a>
+                    <button onClick={onClose} className="px-4 py-2 text-xs md:text-base">Clear</button>
                     <Button onClick={handleSave}>Save</Button>
                 </div>
                 <form className="flex flex-col w-[250px]">
